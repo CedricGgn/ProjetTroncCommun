58f9358873f507f59330b2d315e2f9e743c2f8c9
Lilian2588 <lilian.saglibene1@gmail.com>
2024-04-16 13:49:22 +0200

    login fait

--- conf/postgres/sql/1-acudb-tables.sql
+++ conf/postgres/sql/1-acudb-tables.sql
@@ -1,1 +1,15 @@
-﻿
+﻿CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+
+-- Identifiants de l'admin
+INSERT INTO users (username, password_hash) VALUES ('admin', 'admin');
+
+-- Identifiants des users
+INSERT INTO users (username, password_hash) VALUES ('user1', 'mdp1');
+INSERT INTO users (username, password_hash) VALUES ('user2', 'mdp2');
+INSERT INTO users (username, password_hash) VALUES ('user3', 'mdp3');
+
+
--- src/PHP/login.php
+++ src/PHP/login.php
@@ -2,31 +2,56 @@
 
 session_start();
 
-# Identifiants de connexion de l'utilisateur
-$username = $_POST["username"];
-$password = $_POST["password"];
+// Définir des variables de session
+$_SESSION['username'] = $_POST["username"];
+$_SESSION['password'] = $_POST["password"];
+
+// Accéder aux variables de session
+$username = $_SESSION['username'];
+$password = $_SESSION['password'];
 
 # Identifiants de connexion à la BDD
 $db_host = $_ENV['DB_HOST'];
 $db_name   = $_ENV['DB_DB'];
-$user = $_ENV['DB_USER'];
-$psw = $_ENV['DB_PASSWORD'];
+$user_con = $_ENV['DB_USER'];
+$psw_con = $_ENV['DB_PASSWORD'];
+
 
 # Connexion en utilisant PDO
 try {
-    $dbcon = new PDO("pgsql:host=$db_host;dbname=$db_name", $user, $psw);
-    echo "Connexion réussie \n";
-    $queryusername = "SELECT username FROM \"users\" WHERE username = '$username' ";
-    $querypsw = "SELECT password_hash FROM \"users\" WHERE username = '$username' ";
-    var_dump($_SESSION);
-
+    $dbcon = new PDO("pgsql:host=$db_host;dbname=$db_name", $user_con, $psw_con);
+    //echo "Connexion réussie \n";
 } catch (PDOException $e) {
-    echo "Erreur de connexion : \n" . $e->getMessage();
+    echo "Erreur de connexion à la bdd: \n" . $e->getMessage();
 }
 
+// Requête de selection des infos de la bdd correspondant aux identifiants entrés dans le formulaire
+$queryusername = "SELECT username FROM \"users\" WHERE username = '$username' ";
+$querypsw = "SELECT password_hash FROM \"users\" WHERE username = '$username' ";
+$stmt_user = $dbcon->query($queryusername);
+$stmt_psw = $dbcon->query($querypsw);
 
-$Selectquery = "SELECT username FROM \"users\" WHERE username = $username";
-$stmt = $dbcon->query($Selectquery);
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Aller chercher les info dans la Bdd
+$result_user = $stmt_user->fetchAll(PDO::FETCH_ASSOC);
+$result_psw = $stmt_psw->fetchAll(PDO::FETCH_ASSOC);
 
+// Comparaison mdp et username
+if (count($result_psw) == 0){
+    echo "Nom d'utilisateur incorrect : Connexion échouée";
+}
+else{
+    // Récupérer le mot de passe hashé à partir de la base de données
+    $hashed_password = $result_psw[0]['password_hash'];
+    // Utilisation de password_verify lorsque le mdp est hashé
+    if ($password == $hashed_password ) {
+        if ($username = 'admin'){
+            echo "Connexion à la session administrateur";
+        }
+        else{
+            echo "Connexion à la session utilisateur";
+        }   
+    } else {
+        echo "Mot de passe incorrect : Connexion échouée";
+    }
+}
 ?>

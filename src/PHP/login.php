811aa9931f7ee0a050db30ae8dc4926f83532934
Lilian2588 <lilian.saglibene1@gmail.com>
2024-04-17 11:04:24 +0200

    main.php et connection.php créé fichier commune

--- src/PHP/login.php
+++ src/PHP/login.php
@@ -1,7 +1,6 @@
 <?php
 
 session_start();
-require_once '../vendor/autoload.php';
 
 // Accéder aux variables de session
 $username = $_SESSION['username'];
---
+++ src/PHP/main.php
@@ -0,0 +1,4 @@
+<?php
+require("/PHP/connection.php");
+// Se rendre à la page main 
+ echo $twig->render('main.twig'); 
